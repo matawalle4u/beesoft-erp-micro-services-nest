e00117b0255ce4c063e576403097c32d45ed1efc
Adam Mustapha <matawalle4u@gmail.com>
2026-01-20 19:50:29 +0100

    Injected client with token INVENTORY_SERVICE

--- inventory-client/src/app.controller.ts
+++ inventory-client/src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('INVENTORY_SERVICE') private readonly client: ClientProxy
+  ) {}
 
   @Get()
   getHello(): string {
