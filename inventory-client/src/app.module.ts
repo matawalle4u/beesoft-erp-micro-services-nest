2784b08bb370e4abc7e1d3d2fcca8c42b470dbf8
Adam Mustapha <matawalle4u@gmail.com>
2026-01-20 19:48:49 +0100

    Registered connection to INVENTORY_SERVICE

--- inventory-client/src/app.module.ts
+++ inventory-client/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'INVENTORY_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: 8080,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
